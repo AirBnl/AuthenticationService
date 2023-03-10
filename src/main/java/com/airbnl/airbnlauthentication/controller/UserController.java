7cae594c01c7dc5b81aa62c98f12200f5dcb0985
omar <omar10zoubi@gmail.com>
2023-03-10 22:52:47 +0300

    feat:#2 partially implemented user controller

--- src/main/java/com/airbnl/airbnlauthentication/controller/UserController.java
+++ src/main/java/com/airbnl/airbnlauthentication/controller/UserController.java
@@ -1,9 +1,26 @@
 package com.airbnl.airbnlauthentication.controller;
 
+import com.airbnl.airbnlauthentication.model.User;
+import com.airbnl.airbnlauthentication.service.interfaces.IUserService;
+import lombok.AllArgsConstructor;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("/user")
+@AllArgsConstructor
 public class UserController {
+    private final IUserService userService;
+    @PostMapping
+    public String signIn(User user) {
+        return user.getUsername();
+    }
+
+    @PostMapping
+    public String signUp(User user, Model model) {
+        userService.save(user);
+        return user.getPassword();
+    }
 }
